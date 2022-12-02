2d4b716d15f99bd1380897172e50a9a2af6f181a
Jose-V12 <jv123144@gmail.com>
2022-12-02 08:36:41 -0600

    Created mongoose model

---
+++ Server/Model/RosterInformation.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const playerSchema = new Schema({
+    firstName:{
+        type: String,
+        require: true,
+    },
+    lastName:{
+        type: String,
+        require: true,
+    },
+    position:{
+        type: String,
+        require: true,
+    },
+    year_slary:{
+        type: String,
+        require: true,
+    },
+    leagueYrs:{
+        type: Number,
+        require: true,
+    },
+    injuryReserve:{
+        type: Boolean,
+    }
+})
+module.exports = mongoose.model("RosterInformation", playerSchema);
