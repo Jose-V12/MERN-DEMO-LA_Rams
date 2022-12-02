0605f9ebb3fd1a146faab1638bdcdc75720e4ded
Jose-V12 <jv123144@gmail.com>
2022-12-02 09:04:26 -0600

    linked apps to server

--- Server/index.js
+++ Server/index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 app.use(express.json());
 const cors = require("cors");
 app.use(cors());
+const router = require("./Routes/player-Routes");
+app.use("/LA-Rams", router);
 mongoose.connect("mongodb+srv://Jose-V:Josevargas123144!@cluster0.qwyfktd.mongodb.net/LA_Rams?retryWrites=true&w=majority"
 ).then(() => console.log("Connected To Database"))
 .then(() => {
